d50c1585d7adb226987539bfd938605f2d6d6e09
VindhyaPenna <118455671+VindhyaPenna@users.noreply.github.com>
2023-10-22 08:33:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,14 @@
 //your JS code here. If required.
 let text = document.querySelector("#status");
         let btn = document.getElementById("enterBtn");
+
+
         btn.addEventListener('click',()=>{
         let finText = document.createElement("h1");
 			finText.setAttribute("id","status")
         finText.innerHTML = "Entered Metaverse";
         text.remove();
-        let parent = document.getElementById("body");
+        let parent = document.getElementById("h1#status");
         parent.insertBefore(finText,btn)
         // console.log(parent.children)
         })
