99c1992860fe1aeb455a5ae36a387dce78974393
VindhyaPenna <118455671+VindhyaPenna@users.noreply.github.com>
2023-10-22 08:14:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let text = document.getElementById("status");
+        let btn = document.getElementById("enterBtn");
+        btn.addEventListener('click',()=>{
+        let finText = document.createElement("h1");
+        finText.innerHTML = "Enetered Metaverse";
+        text.remove();
+        let parent = document.getElementById("body");
+        parent.insertBefore(finText,btn)
+        // console.log(parent.children)
+        })
